6e9386451c3c72e1bff2f5b2d0ef1abbde08e338
himanshu-sharmavv <himan9506492198@gmail.com>
2025-11-10 15:46:34 +0530

    fix: Remove conditional basePath to fix client-side routing

--- next.config.mjs
+++ next.config.mjs
@@ -5,13 +5,11 @@ const withMDX = mdx({
   options: {},
 });
 
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/portfolio' : '',
-  assetPrefix: isProd ? '/portfolio' : '',
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio',
   eslint: {
     ignoreDuringBuilds: true,
   },
